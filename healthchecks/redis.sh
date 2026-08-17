830c0327ccabe0600b52ae603b15eadc2d49fe3b
JosAbaafe <billmanuel9@gmail.com>
2026-08-17 16:46:56 +0000

    Editing

--- healthchecks/redis.sh
+++ healthchecks/redis.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -eo pipefail
 
-host="$(hostname -i || echo '127.0.0.1')"
-
-if ping="$(redis-cli -h "$host" ping)" && [ "$ping" = 'PONG' ]; then
-	exit 0
+# Ping Redis directly on the local interface
+if [ "$(redis-cli -h 127.0.0.1 ping)" = 'PONG' ]; then
+        exit 0
 fi
 
 exit 1
+
